826f90a4479732e8c7b49200b6c84792edca0477
gersonch <78560393+gersonch@users.noreply.github.com>
2024-12-30 10:03:41 -0300

    rendering pokemon

--- src/app/dashboard/pokemon/page.tsx
+++ src/app/dashboard/pokemon/page.tsx
@@ -1,16 +1,47 @@
-const getPokemon = async (limit = 20, offset = 0) => {
-  const data = await fetch(
+import { PokemonResponse } from "@/app/pokemon/interfaces/pokemon-response";
+import { SimplePokemon } from "@/app/pokemon/interfaces/simple-pokemon";
+import Image from "next/image";
+
+const getPokemon = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => {
+  const data: PokemonResponse = await fetch(
     `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
   ).then((res) => res.json());
-  return data;
+
+  const pokemon = data.results.map((aPokemon) => ({
+    id: aPokemon.url.split("/").at(-2)!,
+    name: aPokemon.name,
+  }));
+  return pokemon;
 };
 
 export default async function PokemonPage() {
-  const { results } = await getPokemon(150);
+  const pokemon = await getPokemon(150);
   return (
     <>
       <h1>Pokemon page</h1>
-      {JSON.stringify(results)}
+      <section>
+        <div className="flex flex-wrap justify-center items-center gap-4  p-16">
+          {pokemon.map((aPokemon) => (
+            <div
+              key={aPokemon.id}
+              className="p-8 bg-white flex flex-col items-center w-72 h-80 justify-around rounded-lg shadow-lg m-8"
+            >
+              <h2>
+                {String(aPokemon.name).charAt(0).toUpperCase() +
+                  String(aPokemon.name).slice(1)}
+              </h2>
+              <div>
+                <Image
+                  width={100}
+                  height={100}
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${aPokemon.id}.svg`}
+                  alt="pokemon"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
